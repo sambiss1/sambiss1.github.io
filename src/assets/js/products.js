0bc5ef133ac3cb71d95ca7182f08dbc13e33eca2
Sam <sambisselele@gmail.com>
2022-05-17 11:41:25 +0100

    Feature : - Adding background slider on top category section ; 	  - Adding shop page

--- src/assets/js/products.js
+++ src/assets/js/products.js
@@ -2,17 +2,7 @@ const productName = document.querySelectorAll(".productName");
 const productPrice = document.querySelectorAll(".productPrice");
 const productImage = document.querySelectorAll(".productImage");
 const showProduct = () => {
-    const product = {
-
-        id: 1, name: "Men shoes", price: "250", picture: ("src/components/images/men-shoes-1.jpg")
-    };
-    /*productName.innerText = product.name;
-    productPrice.innerText = product.price + " $";
-    productImage.src = product.picture;
-    console.log(product.name + " " + product.price);
-    console.log(productName, productImage, productPrice);
-    */
-    const allProducts = [
+    /*const allProducts = [
 
         { id: 1, name: "Eyes Glasses", price: 200, picture: ("src/components/images/sunglasses.jpg") },
         { id: 2, name: "Hat", price: 300, picture: ("src/components/images/hat-all.jpg") },
@@ -33,5 +23,53 @@ const showProduct = () => {
     for (let i = 0; i < productByCategory.length; i++) {
         productName[i].innexText = productByCategory[i].homme.name;
         console.log(productName[i].textContent);
-    }
+    }*/
 }
+document.addEventListener('DOMContentLoaded', showSomeProduct = () => {
+    const allProducts = [
+        { id: 1, name: "Eyes Glasses", price: 200, picture: ("src/components/images/sunglasses.jpg"), category: "Accesoires" },
+        { id: 2, name: "Hat", price: 300, picture: ("src/components/images/hat-all.jpg"), category: "Accesoires" },
+        { id: 3, name: "Women hat", price: 150, picture: ("src/components/images/women-hat-2.jpg"), category: "Accessoires" },
+        { id: 4, name: "Men Sweat Shirt", price: 100, picture: ("src/components/images/man-model.jpg"), category: "Vétêments" }
+    ]
+    for (let i = 0; i < allProducts.length; i++) {
+        if (productName.length < allProducts.length) {
+            productName[i].innerText = allProducts[i].name;
+            productPrice[i].innerText = allProducts[i].price + " $";
+            productImage[i].src = allProducts[i].picture;
+
+        }
+        else {
+            return;
+        }
+
+
+
+    }
+    /*
+        w3.displayObject("recentlyAdded", recentlyAddedProduct);
+        w3.displayObject("accessoires01", category);
+    */
+})
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+        dots: true,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    })
+});
+
